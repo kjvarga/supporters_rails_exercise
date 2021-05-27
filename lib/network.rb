498339016f4c4d181fe0c471afbf7d8159eb98c4
Karl Varga <kjvarga@gmail.com>
2021-05-27 13:55:48 -0700

    Fix path to network mocks file

--- lib/network.rb
+++ lib/network.rb
@@ -2,6 +2,6 @@ module Network
   def self.fetch_by_user_id(user_id)
     request_time_ms = Kernel.rand(200).to_f / 1000
     Kernel.sleep(request_time_ms)
-    return JSON.parse(File.read(Rails.root.join('app/network-mock.json')))
+    return JSON.parse(File.read(Rails.root.join('app/mocks/network-mock.json')))
   end
 end
